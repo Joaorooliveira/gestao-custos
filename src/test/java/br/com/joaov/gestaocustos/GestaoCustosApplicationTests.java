5641f4f076777a9d3037c08d66e0e4ac55a77565
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-06 11:06:38 -0300

    Initial commit

---
+++ src/test/java/br/com/joaov/gestaocustos/GestaoCustosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaov.gestaocustos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoCustosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
