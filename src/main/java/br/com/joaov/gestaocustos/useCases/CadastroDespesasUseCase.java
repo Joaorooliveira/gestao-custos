a53a611531c3c1a21c45d80788bc82719d5c1c04
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-06 12:43:35 -0300

    feat: adiciona nova use case para cadastrar despesa

--- src/main/java/br/com/joaov/gestaocustos/useCases/CadastroDespesasUseCase.java
+++ src/main/java/br/com/joaov/gestaocustos/useCases/CadastroDespesasUseCase.java
@@ -1,4 +1,11 @@
 package br.com.joaov.gestaocustos.useCases;
+import br.com.joaov.gestaocustos.entity.Despesa;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CadastroDespesasUseCase {
+
+    public void execute(Despesa despesa){
+
+    }
 }
