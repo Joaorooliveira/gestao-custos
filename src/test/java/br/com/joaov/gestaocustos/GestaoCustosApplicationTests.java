f6fb3f4f8462bab6e4543c8108be0df34fbb56f4
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-06 11:17:59 -0300

    chore: adiciona configuracoes do banco H2

--- src/test/java/br/com/joaov/gestaocustos/GestaoCustosApplicationTests.java
+++ src/test/java/br/com/joaov/gestaocustos/GestaoCustosApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GestaoCustosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
