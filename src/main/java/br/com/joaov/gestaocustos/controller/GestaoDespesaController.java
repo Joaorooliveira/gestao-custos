f9ecb6ff10760a199c68139c920457036a712e8f
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-06 13:16:35 -0300

    feat(despesa-controller): adiciona endpoint para criar despesa

--- src/main/java/br/com/joaov/gestaocustos/controller/GestaoDespesaController.java
+++ src/main/java/br/com/joaov/gestaocustos/controller/GestaoDespesaController.java
@@ -1,6 +1,13 @@
 package br.com.joaov.gestaocustos.controller;
 
+import br.com.joaov.gestaocustos.custom_messages.ErrorMessage;
+import br.com.joaov.gestaocustos.entity.Despesa;
+import br.com.joaov.gestaocustos.useCases.CadastroDespesasUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GestaoDespesaController {
 
+    @Autowired
+    private CadastroDespesasUseCase cadastroDespesasUseCase;
     @PostMapping("/create")
-    public void create(){
-
-
+    public ResponseEntity<?> create(@RequestBody Despesa despesa){
+        try{
+            var result = cadastroDespesasUseCase.execute(despesa);
+            return ResponseEntity.ok().body(result);
+        }catch (IllegalArgumentException e){
+            var errorMessage= new ErrorMessage(e.getMessage(),"INVALIDD_PARAMS");
+            return ResponseEntity.status(400).body(errorMessage);
+        }
     }
 
 
