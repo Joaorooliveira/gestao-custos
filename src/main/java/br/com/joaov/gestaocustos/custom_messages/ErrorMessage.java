3b6d0bb39f3d75e4d587f1753b4fa03b211508e3
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-06 13:12:19 -0300

    feat(error-message): adiciona classe de mensagem de erro

--- src/main/java/br/com/joaov/gestaocustos/custom_messages/ErrorMessage.java
+++ src/main/java/br/com/joaov/gestaocustos/custom_messages/ErrorMessage.java
@@ -1,4 +1,17 @@
 package br.com.joaov.gestaocustos.custom_messages;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorMessage {
+
+
+    private String message;
+    private String type;
 }
