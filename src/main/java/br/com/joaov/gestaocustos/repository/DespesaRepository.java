af9f893e60fa874b525f959ffb3c1b17d3a51efe
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-06 15:16:19 -0300

    feat(despesa-repository): adiciona metodos para buscar despesa por email e data

--- src/main/java/br/com/joaov/gestaocustos/repository/DespesaRepository.java
+++ src/main/java/br/com/joaov/gestaocustos/repository/DespesaRepository.java
@@ -3,7 +3,12 @@ package br.com.joaov.gestaocustos.repository;
 import br.com.joaov.gestaocustos.entity.Despesa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface DespesaRepository extends JpaRepository<Despesa, Long> {
 
 
+    List<Despesa> findByEmail(String email);
+    List<Despesa> findByEmailAndData(String email, LocalDate data);
 }
