1965283162d28180b0a015664a217422db33c2c1
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-06 13:15:50 -0300

    feat(cadastro-despesas-use-case): adiciona logica para criar despesa

--- src/main/java/br/com/joaov/gestaocustos/useCases/CadastroDespesasUseCase.java
+++ src/main/java/br/com/joaov/gestaocustos/useCases/CadastroDespesasUseCase.java
@@ -1,11 +1,22 @@
 package br.com.joaov.gestaocustos.useCases;
 import br.com.joaov.gestaocustos.entity.Despesa;
+import br.com.joaov.gestaocustos.repository.DespesaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CadastroDespesasUseCase {
 
-    public void execute(Despesa despesa){
+    @Autowired
+    DespesaRepository despesaRepository;
+
+    public Despesa execute(Despesa despesa){
+        if(despesa.getCategoria() == null || despesa.getData() == null || despesa.getDescricao() == null||
+                despesa.getEmail() == null){
+            throw new IllegalArgumentException("Preencher todos os campos");
+        }
+
+        return despesaRepository.saveAndFlush(despesa);
 
     }
 }
