b4d1a72bfc6d0c03b9a5288a43b18b583bac9422
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-06 11:42:20 -0300

    feat(gestao-depesa-controller): adiciona controller para gestao de despesa

--- src/main/java/br/com/joaov/gestaocustos/controller/GestaoDespesaController.java
+++ src/main/java/br/com/joaov/gestaocustos/controller/GestaoDespesaController.java
@@ -1,4 +1,18 @@
 package br.com.joaov.gestaocustos.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/gestao")
+@RestController
 public class GestaoDespesaController {
+
+    @PostMapping("/create")
+    public void create(){
+
+
+    }
+
+
 }
